5054b0054cda568d939be441cae56c0b0a180aed
LaRaye Johnson <lnj2105@gmail.com>
2019-11-01 22:43:16 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
+let state = {numberOfPresents: 0};
+
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
+let
+
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
