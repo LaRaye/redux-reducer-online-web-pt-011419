594042cd3dbc3bfe3d1b9052e431cd4ef3569861
LaRaye Johnson <lnj2105@gmail.com>
2019-11-01 22:47:28 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,7 +3,7 @@ let state = {friends: []};
 export function manageFriends(state, action1, action2){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {...state, numberOfPresents: state.numberOfPresents + 1}
+      return {...state, }
     default:
       return state;
   }
